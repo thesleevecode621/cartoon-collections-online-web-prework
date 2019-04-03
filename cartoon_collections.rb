29f79827c8b5abdb853d646767bad90061b28f58
boruch d <dubywider@gmail.com>
2019-04-03 18:57:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(mall)
-  mall.any? 
+  mall.any? do |mall|
+    mall.length < 4
 end
 
 def find_the_cheese# code an argument here
