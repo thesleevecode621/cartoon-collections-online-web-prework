988afe6fbf5666105a17880c85bdd921dc7175ba
boruch d <dubywider@gmail.com>
2019-04-03 18:54:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,10 @@ end
 
 end
 
-def summon_captain_planet(array)
- array.collect do |array|
-   array.capitalize << "!"
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
